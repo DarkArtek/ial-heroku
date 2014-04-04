77c3ba3abf263ff3f37797751ea74d081556126f
Marco Paolini <marco@credra.com>
2014-04-04 12:35:10 +0200

    change title

--- onepage/views.py
+++ onepage/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<html><head><title>ONEPAGE</title></head><body><h1>Onepage app</h1></body></html>')
+    return HttpResponse('<html><head><title>ONEPAGE</title></head><body><h1>Onepage app v2</h1></body></html>')
     # return render(request, 'index.html')
 
